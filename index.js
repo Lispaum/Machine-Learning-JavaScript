c34c4e1c95a39af9564fb0c8b44c8711054db8bb
Lispaum <marinofelippe64@gmail.com>
2020-01-19 19:39:01 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+const brain = require('brain.js');
+
+const network = new brain.NeuralNetwork();
+
+/*
+network.train([
+    { input: [0,0,0], output: [0]},
+    { input: [0,0,1], output: [0]},
+    { input: [0,1,1], output: [0]},
+    { input: [1,0,1], output: [1]},
+    { input: [1,1,1], output: [1]}
+]);
+*/
+network.train([
+    { input: [1,0], output: [0]},
+    { input: [2,0], output: [0]},
+    { input: [0,1], output: [1]},
+    { input: [0,1], output: [1]},
+    { input: [2,1], output: [0]}
+]);
+
+const output = network.run([3,0]);
+
+console.log(`Prob: ${output}`);
